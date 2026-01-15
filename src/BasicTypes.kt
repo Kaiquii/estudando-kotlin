81521353182bdeebf55ca1fd0bddf03f1fb17517
Kaiqui <kaiqu@example.com>
2026-01-15 17:59:40 -0300

    2

---
+++ src/BasicTypes.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var customers = 10
+
+    customers += 3 // soma
+    customers -= 9 // subtract
+    customers *= 2 // multiplication
+
+    println(customers)
+}
+
+fun typeAnd() {
+    val d: Int = 10
+    val e: String = "SLA"
+
+    println(e)
+    println(d)
+}
+
+
+
