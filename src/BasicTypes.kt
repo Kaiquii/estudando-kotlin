975d9b094da5d13d60a733ea5f18c45d99320032
Kaiqui <kaiqu@example.com>
2026-01-16 10:47:55 -0300

    Exercise in typing as variables

--- src/BasicTypes.kt
+++ src/BasicTypes.kt
@@ -16,5 +16,14 @@ fun typeAnd() {
     println(d)
 }
 
+fun typeExercises() {
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+}
+
 
 
