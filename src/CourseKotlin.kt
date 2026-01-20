ae88010d37e2f57368b269e02a6e49915eb2a0b0
Kaiqui <kaiqu@example.com>
2026-01-20 17:54:50 -0300

    estudando

---
+++ src/CourseKotlin.kt
@@ -0,0 +1,13 @@
+//fun main() {
+//    val message1: Int = 5
+//    val message2: Int = 10
+//    println("Eu tenho ${message1 + message2} de mensagens na minha caixa de entrada")
+//}
+
+fun main() {
+    val trip1: Double = 3.20
+    val trip2: Double = 4.10
+    val trip3: Double = 1.72
+    val totalTripLength: Double = trip1 + trip2 + trip3
+    println("$totalTripLength milhas restantes até o destino")
+}
