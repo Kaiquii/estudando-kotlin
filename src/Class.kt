f2d4b4ad363ee175b9519d25229b19df0ff5beca
Kaiqui <kaiqu@example.com>
2026-02-10 10:35:11 -0300

    classes em kotlin

--- src/Class.kt
+++ src/Class.kt
@@ -1,3 +1,38 @@
-class Contact(val id: Int, var email: String) {
-    val category: String = ""
+//class Contact(val id: Int, var email: String = "exemplo@gmail.com") {
+//    val category: String = "work"
+//}
+
+class Contact(val id: Int, var email: String)
+
+// Colocar no main
+
+//val contact = Contact(1, "mary@gmail.com")
+//println("O endereço do e-mail é: ${contact.email}")
+
+class ContactMember(val id: Int, var email: String) {
+    fun printId() {
+        println(id)
+    }
 }
+
+// Colocar no main
+
+//val contact = ContactMember(1,  "mary@gmail.com")
+//contact.printId()
+
+data class User(val name: String, val id: Int)
+
+// Colocar no main, usando data class User()
+
+//val user = User("Kaiqui", 1)
+//println(user)
+
+// também pode comparar as instâncias
+
+//val user = User("Alex", 1)
+//val secondUser = User("Alex", 1)
+//val thirdUser = User("Max", 2)
+//
+//println("user == secondUser: ${user == secondUser}")
+//println("user == thirdUser: ${user == thirdUser}")
+//println(user.copy("Max")) // Cria uma cópia e altera dados
--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    maiusculo()
+    val user = User("Alex", 1)
+    val secondUser = User("Alex", 1)
+    val thirdUser = User("Max", 2)
+
+//    println("user == secondUser: ${user == secondUser}")
+//    println("user == thirdUser: ${user == thirdUser}")
+
+    println(user.copy("Max")) // Cria uma cópia e altera dados
 }
